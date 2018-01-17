910ce93f5177d7a026c0d2772499fbf9da1c9827
BratuRobertBeto <32935183+BratuRobertBeto@users.noreply.github.com>
2018-01-17 22:32:34 +0200

    Add files via upload

---
+++ lab 10 I.1.cs
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    FILE* file_to_copy_from;
+    FILE* file_to_copy_in;
+    char c;
+
+    file_to_copy_in = fopen("D:\\Robert C++\\lab10\\CopyIn.txt ", "w");
+
+
+    if ((file_to_copy_from = fopen("D:\\Robert C++\\lab10\\CopyFrom.txt ", "r")) == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
+    else
+
+        do
+        {
+            c = getc(file_to_copy_from);
+
+            if (c >= 'a' && c <= 'z')
+                c = c - 32;
+
+            fputc(c, file_to_copy_in);
+        }
+
+        while (c != EOF);
+
+    fclose(file_to_copy_from);
+    fclose(file_to_copy_in);
+    return 0;
+}
+
+
---
+++ lab10 I.3.cs
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int LineCounter(FILE* file_to_count_from)
+{
+    char c[100];
+    int nr = 0;
+    if ((file_to_count_from = fopen("D:\\Robert C++\\lab10\\CountLines.txt ", "r")) == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
+    else
+
+        while (fscanf(file_to_count_from, "%s", c) != EOF)
+            nr++;
+
+    fclose(file_to_count_from);
+    return nr;
+}
+
+int main()
+{
+    FILE* file_to_count_from;
+    printf("The file has %d lines", LineCounter(file_to_count_from));
+    return 0;
+}
